78b92e06b5a657104171ecabe833443417eaa603
vishuvish <vishvishu14@gmail.com>
2018-08-27 10:00:01 +0530

    https://www.hackerrank.com/contests/projecteuler/challenges/euler067

---
+++ p67.py
@@ -0,0 +1,18 @@
+for _ in range(int(input())):
+    d = dict()
+    n = int(input())
+    for i in range(n):
+        arr = list(map(int, input().split()))
+        for j in range(i+1):
+            d[(i,j)] = arr[j]
+
+# print(d)
+
+    ans = 0
+    for i in range(n-2,-1,-1):
+        for j in range(i+1):
+            maxi = max(d[(i+1, j)], d[(i+1, j+1)])
+            d[(i, j)] += maxi
+
+    # print(d)
+    print(d[(0,0)])
